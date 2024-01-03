85fc88f4a0b3f41a19d0917a3b54dbeacc723a19
André Donadon <andreltdonadon@gmail.com>
2024-01-02 22:51:29 -0300

    Change utils to READ JSON

--- cmd/api/utils.go
+++ cmd/api/utils.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
+	"io"
 	"net/http"
 )
 
@@ -29,5 +31,25 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data interf
 	if err != nil {
 		return err
 	}
+	return nil
+}
+
+func (app *application) readJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
+	maxBytes := 1024 * 1024 // one megabyte
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
+	}
+
 	return nil
 }
